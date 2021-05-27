f4d7991947ffb13038bb8d78252513a241241042
“PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-05-27 12:41:14 -0500

    Second Example

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ alert("At this moments, the functions value is:   "+firstValue+"  So, its not ol
 alert("By adding a value to the function created, the inner function can use the inner and outer values to calculate..."+firstValue(parseInt(document.getElementById("secondValue").value,10)));
 }
 
+function makeSizer(size) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+var size12 = makeSizer(12);
+var size14 = makeSizer(14);
+var size16 = makeSizer(16);
+
+document.getElementById('size-12').onclick = size12;
+document.getElementById('size-14').onclick = size14;
+document.getElementById('size-16').onclick = size16;
